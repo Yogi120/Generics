f861e49a91aaae29e59523a3e5a6a0422fc9e913
Yogesh <me.yogesh.pal@gmail.com>
2023-06-18 20:43:53 +0530

    Generics problem 1

---
+++ Generics4/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Generics4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 3, 6, 4 };
+            int min = arr[0];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[1] < min)
+                {
+                    min = arr[i];
+                }
+
+            }
+            Console.WriteLine("Minimum num " + min);
+        }
+    }
+}
