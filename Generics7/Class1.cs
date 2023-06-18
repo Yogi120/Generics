32016b532aa805c0eae1204bce4c645e31e1f6ab
Yogesh <me.yogesh.pal@gmail.com>
2023-06-18 20:51:54 +0530

    Generics Problem 7

---
+++ Generics7/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics7
+{
+    public class MinimumFinder<T> where T : IComparable<T>
+    {
+        public T FindMinimum(T variable1, T variable2, T variable3)
+        {
+            T min = variable1;
+            if (variable2.CompareTo(min) < 0)
+            {
+                min = variable2;
+            }
+            if (variable3.CompareTo(min) < 0)
+            {
+                min = variable3;
+            }
+            return min;
+        }
+    }
+}
---
+++ Generics7/Program.cs
@@ -0,0 +1,19 @@
+﻿
+namespace Generics7
+{
+
+    public class programm
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 4, num2 = 2, num3 = 5;
+
+            MinimumFinder<int> minimumfind = new MinimumFinder<int>();
+            int min = minimumfind.FindMinimum(num1, num2, num3);
+
+
+            Console.WriteLine(min);
+        }
+
+    }
+}
