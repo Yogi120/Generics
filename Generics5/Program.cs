47c6c7f2ceb87111a7b1f2a5377ac342612ed6c0
Yogesh <me.yogesh.pal@gmail.com>
2023-06-18 20:46:55 +0530

    Generics problem 5

---
+++ Generics5/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Generics5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float[] Num = { 3.8F, 4.9F, 9.2F };
+
+            float min = Num[0];
+
+            for (int i = 1; i < Num.Length; i++)
+            {
+                if (Num[1] < min)
+                {
+                    min = Num[i];
+                }
+
+            }
+            Console.WriteLine("Minimum number is " + min);
+        }
+    }
+}
