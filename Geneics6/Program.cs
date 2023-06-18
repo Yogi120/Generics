c12be13a2cfda9468e54270c6946e63d0d4f8e40
Yogesh <me.yogesh.pal@gmail.com>
2023-06-18 20:49:01 +0530

    Generics problem 6

---
+++ Geneics6/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Geneics6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] str = { "cars", "bikes", "trains" };
+
+            string min = str[0];
+
+            for(int i = 1; i<str.Length; i++)
+            {
+                if (string.Compare(str[i], min) < 0)
+                {
+                    min = str[i];
+                }
+            }
+            Console.WriteLine("minimum string is " + min);
+        }
+    }
+}
